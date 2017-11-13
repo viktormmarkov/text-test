a06be2062a8ccd0b30c717ad8b64f71ef9ae927f
Victor Markov <vikata3@gmail.com>
2017-11-13 22:27:18 +0200

    Stable basic navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Grid, Navbar, Nav, NavItem, Jumbotron, Button } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const Artists = () => (
+  <div>
+    <h2>Artists</h2>
+  </div>
+)
+
+const Songs = () => (
+  <div>
+    <h2>Songs</h2>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-         <Navbar inverse fixedTop>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="/">Rap Stats</a>
-              </Navbar.Brand>
-              <Nav>
-                <NavItem eventKey={1} href="#">Add artist</NavItem>
-                <NavItem eventKey={2} href="#">Add song</NavItem>
-              </Nav>
-            </Navbar.Header>
-        </Navbar>
-        <Jumbotron>
-        <Grid>
-          <h1>Raps meets raps</h1>
-        </Grid>
-      </Jumbotron>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar inverse fixedTop>
+              <Navbar.Header>
+                <Navbar.Brand>
+                  <a href="/">Rap Stats</a>
+                </Navbar.Brand>
+                <Nav>
+                  <NavItem><Link to="/"></Link>Home</NavItem>
+                  <NavItem><Link to="/artists">Artists</Link></NavItem>
+                  <NavItem><Link to="/songs">Songs</Link></NavItem>
+                </Nav>
+              </Navbar.Header>
+          </Navbar>
+          <Jumbotron>
+          <Grid>
+            <hr/>
+              <Route exact path="/" component={Home}/>
+              <Route path="/artists" component={Artists}/>
+              <Route path="/songs" component={Songs}/>
+          </Grid>
+        </Jumbotron>
+        </div>
+      </Router>
     );
   }
 }
