74687d1c33950598841068ead27cdd721d5758ab
Victor Markov <vikata3@gmail.com>
2017-11-14 23:22:33 +0200

    Missed file

---
+++ client/src/Songs.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Grid, Navbar, Nav, NavItem, Jumbotron, Button } from 'react-bootstrap';
+
+class Songs extends Component {
+  render() {
+    return (
+      <h1>Songs Page</h1>
+    );
+  }
+}
+
+export default Songs;
