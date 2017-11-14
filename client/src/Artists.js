3f3c221885a53ed420f4e6fd5d5924d9f9c06cce
Victor Markov <vikata3@gmail.com>
2017-11-14 23:57:52 +0200

    Display artists list

--- client/src/Artists.js
+++ client/src/Artists.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react';
-import { Grid, Navbar, Nav, NavItem, Jumbotron, Button } from 'react-bootstrap';
+import { Grid, Button } from 'react-bootstrap';
 
 class Artists extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      artists: []
+    };
+  }
+  componentDidMount() {
+    fetch('/artists', {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    }).then(async res => {
+      const artists = await res.json();
+      this.setState({artists});
+    })
+  }
+  
   render() {
+    const {artists} = this.state;
+    const artistList = artists.map((artist, i) => (
+      <tr key={artist._id}>
+       <td>{artist.name}</td>
+       <td>{artist._id}</td>
+      </tr>
+    ));
     return (
-      <h1>Artists Page</h1>
+      <div>
+        <h1>Artists Page</h1>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+            <td>Name</td>
+            <td>ID</td>
+            </tr>
+          </thead>
+          <tbody>
+            {artistList}
+          </tbody>
+        </table>
+        <Button>Add Artist</Button>
+        
+      </div>
     );
   }
 }
