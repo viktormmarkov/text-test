d939f7caa1b0efb24c6a3a7a6f69fff7df5ad124
Victor Markov <vikata3@gmail.com>
2017-11-21 23:27:41 +0200

    Missed file

--- models/artistController.js
+++ models/artistController.js
@@ -14,7 +14,29 @@ function getArtist(req, res) {
     });
 }
 
+function addArtist(req, res) {
+    const artist = new Artist({ name: req.body && req.body.name });
+    return artist.save(function(err, data) {
+        if(err) {
+            console.log(err, 'errorr');
+        }
+        res.send(data); 
+    });
+}
+
+function deleteArtist(req, res) {
+    const id = req.params.id;
+    return Artist.remove({_id: id}, function (err, data) {
+        if(err) {
+            console.log(err, 'errorr');
+        }
+        res.send(data); 
+    });
+}
+
 module.exports = function (rootApp) {
     rootApp.get('/artists/:id', getArtist);    
+    rootApp.delete('/artists/:id', deleteArtist);    
     rootApp.get('/artists', getArtists);
+    rootApp.post('/artists', addArtist);
 }
