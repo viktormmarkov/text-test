17d0bb7d53a89117c2e169bdfe5183a647b0ae09
Victor Markov <vikata3@gmail.com>
2017-11-22 00:50:29 +0200

    Add artist form

--- client/src/Artists.js
+++ client/src/Artists.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Button } from 'react-bootstrap';
+import { Grid, Row, Col, Button, FormControl, FormGroup, ControlLabel, HelpBlock} from 'react-bootstrap';
 import { Route, Switch, Link } from 'react-router-dom'
 
 class AllArtists extends Component {
@@ -49,12 +49,61 @@ class AllArtists extends Component {
   }
 }
 
-const AddArtist = () => (
-  <div>
-    <h1>Add artist</h1>
-    <Button><Link to="/artists">Add Artist</Link></Button>
-  </div>
-)
+class AddArtist extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    console.log(event.target.value);
+    this.setState({name: event.target.value});
+  }
+
+  handleSubmit(event) {
+    this.addArtist();
+    event.preventDefault();
+  }
+
+  addArtist() {
+    const name = this.state.name || '';
+    fetch('/artists', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({name})
+    });
+  }
+
+  render() {
+    return (
+      <div>
+      <h1>Add artist</h1>
+      <form onSubmit={this.handleSubmit}>
+        <FormGroup
+          controlId="formBasicText"
+        >
+          <ControlLabel>Name</ControlLabel>
+          <FormControl
+            bsSize="lg"
+            type="text"
+            value={this.state.name}
+            placeholder="Aritst name"
+            onChange={this.handleChange}
+          />
+        </FormGroup>
+        <Button type="submit">Add</Button>
+      </form>
+      </div>
+    )
+  }
+}
 class Artists extends Component {
   render() {
     return (
